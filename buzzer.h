416f83de139cb5a403fa5a29aeff458c7b3b839d
Jose Mariano Barrios <thetreeman2003@gmail.com>
2025-12-08 19:18:32 -0800

    Create buzzer.h

---
+++ buzzer.h
@@ -0,0 +1,16 @@
+/*
+ * buzzer.h
+ *
+ *  Created on: Dec 6, 2025
+ *      Author: josebarrios
+ */
+
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+#include "stm32l476xx.h"
+
+// Initialization of GPIO Port D for the buzzer
+void GPIOD_Initialization(void);
+
+#endif /* BUZZER_H_ */
