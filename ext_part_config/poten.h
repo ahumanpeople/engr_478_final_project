a89b9d459aab863200302b4c74946dda115edd9f
Jose Mariano Barrios <thetreeman2003@gmail.com>
2025-12-13 17:09:18 -0800

    Create poten.h

---
+++ ext_part_config/poten.h
@@ -0,0 +1,16 @@
+/*
+ * poten.h
+ *
+ *  Created on: Dec 6, 2025
+ *      Author: josebarrios
+ */
+
+#ifndef POTEN_H_
+#define POTEN_H_
+
+#include "stm32l476xx.h"
+
+// Initialization of GPIO Port B for the potentiometer
+void GPIOB_Initialization(void);
+
+#endif /* POTEN_H_ */
