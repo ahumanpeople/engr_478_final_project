92d4e9c4756f5512bc02ad4e658602e8abc41c42
Jose Mariano Barrios <thetreeman2003@gmail.com>
2025-12-14 16:48:31 -0800

    Update main.c
    - updated after changing code so LEDs would function

--- ext_part_config/main.c
+++ ext_part_config/main.c
@@ -5,31 +5,35 @@
 
 // Include all necessary header files
 #include "stm32l476xx.h"
-// #include "LED.h"
+#include "LED.h"
 // #include "systick.h"
 // #include "buzzer.h"
 // #include "poten.h"
 // #include "button.h"
 
 /* Define macros for the pin numbers connected to external components */
-#define RED_LED 9		// PA5 presents conflict with on-board LED
-#define YELLOW_LED 6
-#define GREEN_LED 7
-#define POTEN 3
-#define SET_BUTTON 14	// PC13 presented conflict with on-board button
-#define PED_BUTTON 15
-#define BUZZER 2
+#define RED_LED		9	// PA5 presents conflict with on-board LED
+#define YELLOW_LED	6
+#define GREEN_LED	7
+#define POTEN		3
+#define SET_BUTTON	14	// PC13 presented conflict with on-board button
+#define PED_BUTTON	15
+#define BUZZER		2
 
-#define BUZZER_FREQ 44
+#define BUZZER_FREQ	44
 
 int main(void)
 {
-	// GPIOA_Initialization();
+	GPIOA_Initialization();
 	// GPIOB_Initialization();
 	// GPIOC_Initialization();
 	// GPIOD_Initialization();
 
 	// SysTick_Init(4000000);
+
+	turn_on_LED(YELLOW_LED);
+	turn_on_LED(RED_LED);
+	turn_on_LED(GREEN_LED);
 
 	while (1) { /* while loop is true; infinite loop */ }
 }
