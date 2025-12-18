13eeaeb999d6916e8df071ebe3c458e2ca918d8e
Jose Mariano Barrios <thetreeman2003@gmail.com>
2025-12-18 10:55:39 -0800

    Update systick.c

--- systick.c
+++ systick.c
@@ -6,17 +6,20 @@
  */
 
 #include "stm32l476xx.h"
-#include "LED.h"
+#include "initialize.h"
 #include "systick.h"
-/*
-#include "buzzer.h"
-*/
 
 /* Define macros for the pin numbers connected to external components */
-#define RED_LED 9
-#define YELLOW_LED 6
-#define GREEN_LED 7
-#define BUZZER 2
+#define RED_LED		9	// PA5 presents conflict with on-board LED
+#define YELLOW_LED	6
+#define GREEN_LED	7
+#define POTEN		0	// Modified from PB3 to PB0 since PB0 is wired to ADC12_IN15
+#define ADCWIRE		15	// PB0 is wired to ADC12_IN15, so a macro was created to facilitate a proper connection
+#define SET_BUTTON	10	// PC13 presented conflict with on-board button
+#define PED_BUTTON	12
+#define BUZZER		2
+
+
 /* Define macros for the following constants used in this file */
 #define TICKINT 1
 // 1 = counting down to zero asserts the SysTick exception request
@@ -24,16 +27,18 @@
 #define CLKSOURCE 1
 // If CLKSOURCE = 0, the external clock is used. The frequency of SysTick clock is the frequency of the AHB clock divided by 8.
 // If CLKSOURCE = 1, the processor clock is used.
-#define RED_DURATION 1000
-#define GREEN_DURATION 1000
-#define YELLOW_DURATION 1000
-// Assume value to be in milliseconds; amount of time each LED should be lit up for
 
-static volatile uint32_t count_ms = 0; 			// ms count the amount of time that has past
-static volatile uint32_t duration_limit_ms = 0;	// ms count for the next time the program needs to switch light
+volatile uint32_t count_ms = 0; 			// ms count the amount of time that has past
+const uint32_t total_duration = 6000000;	// total duration of the traffic light cycle
+// ^^^ float values to proportionately set the timing for each of the LEDs
+uint32_t red_duration = (total_duration / 20UL) * 9UL;
+uint32_t yellow_duration = total_duration / 200UL;
+uint32_t green_duration = (total_duration / 20UL) * 9UL;
+// ^^^ Assume values to be in milliseconds; amount of time each LED should be lit up for
+volatile uint32_t duration_limit_ms = total_duration / 20UL;	// ms count for the next time the program needs to switch light
 
-enum LED_STATE {RED=1, GREEN, YELLOW};	// An enumeration to easily select which LED is being focused on
-enum LED_STATE current_state = RED; 	// Select the current state to be red; start off with the stop light on
+enum LED_STATE {RED_STATE=1, GREEN_STATE, YELLOW_STATE};	// An enumeration to easily select which LED is being focused on
+enum LED_STATE current_state = RED_STATE; 	// Select the current state to be red; start off with the stop light on
 
 
 void SysTick_Init(uint32_t Reload)
@@ -52,17 +57,44 @@ void SysTick_Init(uint32_t Reload)
 	SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
 }
 
-void State_Init(enum LED_STATE i)
+void State_Init(void)
 {
+	// switches to new state in the sequence
+	uint32_t i = current_state;
+	uint32_t new_duration;
+	if (i == YELLOW_STATE)
+	{
+		i = RED_STATE;
+		new_duration = red_duration;
+	}
+	else if (i == RED_STATE)
+	{
+		i = GREEN_STATE;
+		new_duration = green_duration;
+	}
+	else
+	{
+		i = YELLOW_STATE;
+		new_duration = yellow_duration;
+	}
+
 	switch_LED(i); // switch to the right LED to be lit up
-	current_state = i; //
-	uint32_t new_duration = (i == RED) ? RED_DURATION :
-							(i == GREEN) ? GREEN_DURATION :
-							YELLOW_DURATION;
-	// using the identifier i to select the LED that should be lit up now,
+	/*
+	uint32_t new_duration = (i == RED_STATE) ? red_duration :
+							(i == GREEN_STATE) ? green_duration :
+							(i == YELLOW_STATE) ? yellow_duration :
+							yellow_duration;
+
+	if (i == YELLOW_STATE) { i = RED_STATE; }
+	else if (i == RED_STATE) { i = GREEN_STATE; }
+	else { i = YELLOW_STATE; }
+	*/
+
+	// using the identifier "i" to select the LED that should be lit up now,
 	// add the time it should be lit up for to the limit,
 	// extending the duration limit and leaving the LED on for the desired time
 	duration_limit_ms += new_duration;
+	current_state = i;
 }
 
 void SysTick_Handler(void)
@@ -70,19 +102,6 @@ void SysTick_Handler(void)
 	count_ms++; // adds time to count; should be in 1 ms intervals
 	if ((count_ms - duration_limit_ms) >= 0)
 	{
-		switch (current_state)
-		{
-		case RED:
-			State_Init(RED);
-			break;
-		case GREEN:
-			State_Init(GREEN);
-			break;
-		case YELLOW:
-			State_Init(YELLOW);
-			break;
-		default:
-			switch_LED(5);
-		}
+		State_Init();
 	}
 }
