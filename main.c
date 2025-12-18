335b22aa8b68fbde08452544f92edcf9b60e113d
Jose Mariano Barrios <thetreeman2003@gmail.com>
2025-12-18 10:54:43 -0800

    Update main.c

--- main.c
+++ main.c
@@ -3,52 +3,40 @@
 *	Author: Oscar T. Madong, Jose Barrios
 */
 
-// Include all necessary header files
+/* Include all necessary header files */
 #include "stm32l476xx.h"
-#include "LED.h"
-// #include "systick.h"
-/*
-#include "buzzer.h"
-#include "poten.h"
-#include "button.h"
-*/
+#include "initialize.h"
+#include "systick.h"
+#include "ADC.h"
 
 /* Define macros for the pin numbers connected to external components */
-#define RED_LED 9		// PA5 presents conflict with on-board LED
-#define YELLOW_LED 6
-#define GREEN_LED 7
-#define POTEN 3
-#define SET_BUTTON 14	// PC13 presented conflict with on-board button
-#define PED_BUTTON 15
-#define BUZZER 2
+#define RED_LED		9	// PA5 presents conflict with on-board LED
+#define YELLOW_LED	6
+#define GREEN_LED	7
+#define POTEN		0	// Modified from PB3 to PB0 since PB0 is wired to ADC12_IN15
+#define ADCWIRE		15	// PB0 is wired to ADC12_IN15, so a macro was created to facilitate a proper connection
+#define SET_BUTTON	10	// PC13 presented conflict with on-board button
+#define PED_BUTTON	12
+#define BUZZER		2
 
-#define BUZZER_FREQ 44
-
-volatile unsigned long settings_counter = 0;
-volatile unsigned long traffic_light_cycle = 0;
-
-/*
-void Init_All_GPIO(void)
-{
-	GPIOA_Initialization();
-	GPIOB_Initialization();
-	GPIOC_Initialization();
-	GPIOD_Initialization();
-}
-*/
+volatile uint32_t adc_input = 0;
+volatile uint32_t reload_value = 4000000;
+// extern const uint32_t total_duration = 6000000;
+extern const uint32_t total_duration;
 
 int main(void)
 {
-	GPIOA_Initialization();
-	// GPIOB_Initialization();
-	// GPIOC_Initialization();
-	// GPIOD_Initialization();
+	// Initialization of GPIO Port A for LEDs
+	LED_Initialization();
+	// Initialization of GPIO Port C for push buttons, and their EXTI configuration
+	Configure_GPIO_EXTI_Buttons();
+	// Initialization of GPIO Port D for the buzzer
+	Buzzer_Initialization();
 
-	turn_off_LED(RED_LED);
-	turn_on_LED(YELLOW_LED);
-	turn_on_LED(GREEN_LED);
+	SysTick_Init(reload_value);
 
-	// SysTick_Init(4000000);
+	while (1) /* while loop set to 1 for infinite loop */
+	{
 
-	while (1) { /* while loop is true; infinite loop */ }
+	}
 }
