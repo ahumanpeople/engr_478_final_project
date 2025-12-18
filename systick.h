74e460db2f3e008184af0bcf2a1154699a3366f6
Jose Mariano Barrios <thetreeman2003@gmail.com>
2025-12-18 10:55:53 -0800

    Update systick.h

--- systick.h
+++ systick.h
@@ -9,15 +9,13 @@
 #define SYSTICK_H_
 
 #include "stm32l476xx.h"
-/*
-#include "systick.h"
-#include "LED.h"
-#include "buzzer.h"
-*/
+#include "initialize.h"
+
+extern const uint32_t total_duration;
 
 void SysTick_Init(uint32_t Reload);
 
-void State_Init(enum LED_STATE i);
+void State_Init(void);
 
 void SysTick_Handler(void);
 
